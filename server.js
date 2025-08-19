b57626601882205bd03dd4a7541f3d4d6d054e4b
stdrouuu <brajs0111@gmail.com>
2025-08-19 12:32:54 +0700

    method/verb request

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
+// 1. builidng HTTP server
+// const http = require("http");
+
+// const requestListener = (request, response) => {
+//   response.setHeader("Content-Type", "text/html");
+
+//   response.statusCode = 200;
+//   response.end("<h1>Halo HTTP Server!</h1>");
+// };
+
+// const server = http.createServer(requestListener);
+
+// const port = 5000;
+// const host = "localhost";
+
+// server.listen(port, host, () => {
+//   console.log(`Server berjalan pada http://${host}:${port}`);
+// });
+// to see response, run terminal/cmd: curl -X GET http://localhost:5000/
+
+
+
+// 2. Method/Verb Request (GET, POST, PUT, DELETE)
 const http = require("http");
 
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
-
   response.statusCode = 200;
-  response.end("<h1>Halo HTTP Server!</h1>");
+
+  const { method } = request;
+
+  if (method === "GET") {
+    response.end("<h1>Hello!</h1>");
+  }
+
+  if (method === "POST") {
+    response.end("<h1>Hai!</h1>");
+  }
+
+  if (method === "PUT") {
+    response.end("<h1>Bonjour!</h1>");
+  }
+
+  if (method === "DELETE") {
+    response.end("<h1>Salam!</h1>");
+  }
 };
 
 const server = http.createServer(requestListener);
@@ -16,4 +55,7 @@ server.listen(port, host, () => {
   console.log(`Server berjalan pada http://${host}:${port}`);
 });
 
-// to see response, run terminal command: curl -X GET http://localhost:5000/
+// curl -X GET http://localhost:5000 ==> output: <h1>Hello!</h1>
+// curl -X POST http://localhost:5000 ==> output: <h1>Hai!</h1>
+// curl -X PUT http://localhost:5000 ==> output: <h1>Bonjour!</h1>
+// curl -X DELETE http://localhost:5000 ==> output: <h1>Salam!</h1>
