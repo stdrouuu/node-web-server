f5e22293de33ceb6e552c0af1f419f46caa8f187
stdrouuu <brajs0111@gmail.com>
2025-08-19 11:06:38 +0700

    building HTTP server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Learning how to create a server in Node.js!");
+const http = require("http");
+
+const requestListener = (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+
+  response.statusCode = 200;
+  response.end("<h1>Halo HTTP Server!</h1>");
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
+
+// to see response, run terminal command: curl -X GET http://localhost:5000/
