6258425b4b362c6076314ffab259f0688d733843
stdrouuu <brajs0111@gmail.com>
2025-08-19 13:47:11 +0700

    response header

--- server.js
+++ server.js
@@ -168,50 +168,72 @@
 // 400 = client error (bad request, method salah)
 // 404 = not found
 // 500 = server error
+// const http = require("http");
+
+// const requestListener = (request, response) => {
+//   response.setHeader("Content-Type", "text/html");
+
+//   const { method, url } = request;
+
+//   if (url === "/") {
+//     if (method === "GET") {
+//       response.statusCode = 200;
+//       response.end("<h1>Ini adalah homepage</h1>");
+//     } else {
+//       response.statusCode = 400;
+//       response.end(
+//         `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+//       );
+//     }
+//   } else if (url === "/about") {
+//     if (method === "GET") {
+//       response.statusCode = 200;
+//       response.end("<h1>Halo! Ini adalah halaman about</h1>");
+//     } else if (method === "POST") {
+//       let body = [];
+
+//       request.on("data", (chunk) => {
+//         body.push(chunk);
+//       });
+
+//       request.on("end", () => {
+//         body = Buffer.concat(body).toString();
+//         const { name } = JSON.parse(body);
+//         response.statusCode = 200;
+//         response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+//       });
+//     } else {
+//       response.statusCode = 400;
+//       response.end(
+//         `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
+//       );
+//     }
+//   } else {
+//     response.statusCode = 404;
+//     response.end("<h1>Halaman tidak ditemukan!</h1>");
+//   }
+// };
+
+// const server = http.createServer(requestListener);
+
+// const port = 5000;
+// const host = "localhost";
+
+// server.listen(port, host, () => {
+//   console.log(`Server berjalan pada http://${host}:${port}`);
+// });
+// to see response, run terminal/cmd: --------------------------------------------------------------------
+//curl -X GET http://localhost:5000/about -i ==> output: HTTP/1.1 200 OK
+// curl -X GET http://localhost:5000/test -i ==> output: 404 Not Found
+// curl -X DELETE http://localhost:5000/ -i ==> output: 400 Bad Request
+
+// 5. Response Header -------------------------------------------------------------------------------------
+// header =  tambahan untuk response, misal content type(JSON, HTML, dll), powered-by. bs akses pk response.setHeader
 const http = require("http");
 
 const requestListener = (request, response) => {
-  response.setHeader("Content-Type", "text/html");
-
-  const { method, url } = request;
-
-  if (url === "/") {
-    if (method === "GET") {
-      response.statusCode = 200;
-      response.end("<h1>Ini adalah homepage</h1>");
-    } else {
-      response.statusCode = 400;
-      response.end(
-        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
-      );
-    }
-  } else if (url === "/about") {
-    if (method === "GET") {
-      response.statusCode = 200;
-      response.end("<h1>Halo! Ini adalah halaman about</h1>");
-    } else if (method === "POST") {
-      let body = [];
-
-      request.on("data", (chunk) => {
-        body.push(chunk);
-      });
-
-      request.on("end", () => {
-        body = Buffer.concat(body).toString();
-        const { name } = JSON.parse(body);
-        response.statusCode = 200;
-        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
-      });
-    } else {
-      response.statusCode = 400;
-      response.end(
-        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
-      );
-    }
-  } else {
-    response.statusCode = 404;
-    response.end("<h1>Halaman tidak ditemukan!</h1>");
-  }
+  response.setHeader("Content-Type", "application/json");
+  response.setHeader("Powered-By", "Node.js");
 };
 
 const server = http.createServer(requestListener);
@@ -223,6 +245,4 @@ server.listen(port, host, () => {
   console.log(`Server berjalan pada http://${host}:${port}`);
 });
 // to see response, run terminal/cmd: --------------------------------------------------------------------
-//curl -X GET http://localhost:5000/about -i ==> output: HTTP/1.1 200 OK
-// curl -X GET http://localhost:5000/test -i ==> output: 404 Not Found
-// curl -X DELETE http://localhost:5000/ -i ==> output: 400 Bad Request
+// curl -X GET http://localhost:5000 -i ==> output: content-type: application/json, powered-by: Node.js
