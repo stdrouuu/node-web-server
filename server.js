0c67a5f11c5987935d69aef57be3444c1374c09a
stdrouuu <brajs0111@gmail.com>
2025-08-19 13:17:12 +0700

    routing request

--- server.js
+++ server.js
@@ -19,7 +19,6 @@
 // to see response, run terminal/cmd: ---------------------------------------------------
 // curl -X GET http://localhost:5000/
 
-
 // 2. Method/Verb Request (GET, POST, PUT, DELETE)---------------------------------------
 // const http = require("http");
 
@@ -60,32 +59,84 @@
 // curl -X PUT http://localhost:5000 ==> output: <h1>Bonjour!</h1>
 // curl -X DELETE http://localhost:5000 ==> output: <h1>Salam!</h1>
 
-
 // 3. Body Request -----------------------------------------------------------------------
+// const http = require("http");
+
+// const requestListener = (request, response) => {
+//   response.setHeader("Content-Type", "text/html");
+//   response.statusCode = 200;
+
+//   const { method } = request;
+
+//   if (method === "GET") {
+//     response.end("<h1>Hello!</h1>");
+//   }
+
+//   if (method === "POST") {
+//     let body = [];
+
+//     request.on("data", (chunk) => {
+//       body.push(chunk);
+//     });
+
+//     request.on("end", () => {
+//       body = Buffer.concat(body).toString();
+//       const { name } = JSON.parse(body); //JSON.parse to convert string to javascript object
+//       response.end(`<h1>Hai, ${name}!</h1>`);
+//     });
+//   }
+// };
+
+// const server = http.createServer(requestListener);
+
+// const port = 5000;
+// const host = "localhost";
+
+// server.listen(port, host, () => {
+//   console.log(`Server berjalan pada http://${host}:${port}`);
+// });
+// to see response, run terminal/cmd: --------------------------------------------------------------------
+// curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"Brandon\"}" ==> output: <h1>Hai, Brandon!</h1>
+
+// 4. Routing Request ------------------------------------------------------------------------------------
 const http = require("http");
 
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
   response.statusCode = 200;
 
-  const { method } = request;
+  const { method, url } = request;
 
-  if (method === "GET") {
-    response.end("<h1>Hello!</h1>");
-  }
+  if (url === "/") {
+    if (method === "GET") {
+      response.end("<h1>Ini adalah homepage</h1>");
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end("<h1>Halo! Ini adalah halaman about</h1>");
+    } else if (method === "POST") {
+      let body = [];
 
-  if (method === "POST") {
-    let body = [];
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
 
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body); //JSON.parse to convert string to javascript object
-      response.end(`<h1>Hai, ${name}!</h1>`);
-    });
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
+      );
+    }
+  } else {
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
 
@@ -98,4 +149,7 @@ server.listen(port, host, () => {
   console.log(`Server berjalan pada http://${host}:${port}`);
 });
 // to see response, run terminal/cmd: --------------------------------------------------------------------
-// curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"Brandon\"}" ==> output: <h1>Hai, Brandon!</h1>
+// curl -X GET http://localhost:5000/about ==> output: <h1>Halo! Ini adalah halaman about</h1>
+// curl -X POST -H "Content-Type: application/json" http://localhost:5000/about -d "{\"name\": \"Dicoding\"}" ==> output: <h1>Halo, Dicoding! Ini adalah halaman about</h1>
+// curl -X PUT http://localhost:5000/about ==> output: <h1>Halaman tidak dapat diakses menggunakan PUT request</h1>
+// curl -X DELETE http://localhost:5000/about ==> output: <h1>Halaman tidak dapat diakses menggunakan DELETE request</h1>
